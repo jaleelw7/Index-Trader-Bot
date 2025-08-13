3d59acf562caf701769438e10bf22c96a0712d18
Jaleel Williams <jaleel.williams1@bison.howard.edu>
2025-08-13 13:47:14 -0500

    Added data processing file with ticker_exists method.

---
+++ data_processing.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import pandas as pd
+import talib
+import time
+
+
+"""
+"""
+"""
+Method to download the data from each index
+"""
+def download_data(ticker: str) -> pd.DataFrame:
+  pass
