550e62a027460f423bb9fed0dab5bfd71c5d20d1
Jaleel Williams <jaleel.williams1@bison.howard.edu>
2025-08-13 13:51:43 -0500

    Changes for previous commit were not saved. Added data_processing.py with ticker_exists method.

--- data_processing.py
+++ data_processing.py
@@ -5,9 +5,18 @@ import time
 
 
 """
+Method to verify if a ticker exists
 """
+def ticker_exists(ticker: str) -> bool:
+  try:
+    info = yf.Ticker(ticker).info
+    return "shortName" in info and bool(info["shortName"])
+  except Exception:
+    return False
+  
 """
 Method to download the data from each index
 """
 def download_data(ticker: str) -> pd.DataFrame:
-  pass
+  if not ticker_exists(ticker):
+    raise ValueError(f"{ticker} does not exist.")
