613931e16e8567718b34fc40bf483db292e92d1c
Jaleel Williams <jaleelcollin18@gmail.com>
2025-08-27 20:49:15 -0400

    Created TCNModel class

--- model/model_tcn.py
+++ model/model_tcn.py
@@ -53,3 +53,46 @@ class ResidualBlock(nn.Module):
       x = self.identity_conv(x)
     
     return nn.LeakyReLU(x + out)
+
+class TCNModel(nn.Module):
+  """
+  Class for TCN model subclassing nn.Module, made from a stack of ResidualBlock objects.
+
+  Predicts one label per time window
+  """
+  def __init__(self, 
+               in_size: int, 
+               n_filters: list[int], 
+               kernel_sizes: list[int], 
+               dilations: list[int],
+               n_classes: int):
+    super().__init__()
+    # Checks if n_filters, kernel_sizes, and dilations have the same length. Raises a ValueError if False
+    if not(len(n_filters) == len(kernel_sizes) == len(dilations)):
+      raise ValueError("n_filters, kernel_sizes, and dilations must have the same length")
+    
+    res_blocks = [] # List of residual blocks
+    # Adds each residual block to the list
+    for i in range(len(n_filters)):
+      in_c = in_size if i == 0 else n_filters[i-1] # Number of input channels for block i
+      out_c = n_filters[i] # Number of output channels for block i
+      res_blocks.append(ResidualBlock(in_size=in_c, out_size=out_c, kernel_size=kernel_sizes[i], dilation=dilations[i])) # Adds block i to the list
+    
+    self.residual_blocks = nn.Sequential(*res_blocks) # Integrates residual blocks into the model
+    self.head = nn.Conv1d(in_channels=n_filters[-1], out_channels=n_classes, kernel_size=1) # Maps ResidualBlock output to classes
+  
+  def forward(self, x):
+    """
+    Method that defines the forward computation on input
+    """
+    out = self.residual_blocks(x)
+    logit_seq = self.head(out) # Model output per timestep in input sequence
+    return logit_seq[:, :, -1] # Returns model output for the last timestep only
+
+test_model = TCNModel(in_size=3,
+                      n_filters=[16, 16, 32],
+                      kernel_sizes=[3, 3, 3],
+                      dilations=[1, 2, 4],
+                      n_classes=3)
+
+print(test_model)
