6b120707fbd9730450e0c7d864bad1895fbff35e
Jaleel Williams <jaleelcollin18@gmail.com>
2025-08-29 22:33:37 -0400

    Added get_weights() function. Changed get_loaders() to also return class weights if return_weights parameter is True

--- train.py
+++ train.py
@@ -14,10 +14,25 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu" # Sets device to GPU if 
 # Setting RNG seed
 torch.manual_seed(RANDOM_SEED)
 
-def get_loaders() -> tuple[DataLoader, DataLoader]:
+def get_weights(train_labels: np.ndarray):
   """
-  Function to retrieve training and testing data and load
-  it into DataLoaders
+  Function to calculate class weights for training data.
+
+  Weights are inversely proportional to class frequency.
+  """
+  class_counts = np.bincount(train_labels) # List of number of samples per class
+  n_samples = class_counts.sum() # Total number of samples
+  weights = n_samples / (len(class_counts) * class_counts) # Class weights
+  class_weights  = torch.Tensor(weights, dtype=torch.float32, device=DEVICE)
+
+  return class_weights
+
+
+def get_loaders(return_weights=True) -> tuple:
+  """
+  Function to retrieve training and testing data and load it into DataLoaders
+
+  If return_weights parameter is True, also returns class weights
   """
   X_train, X_test, y_train, y_test = get_train_test() # Gets training and testing data
   # Creates dataset objects from training and testing data
@@ -27,7 +42,12 @@ def get_loaders() -> tuple[DataLoader, DataLoader]:
   train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
   test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-  return train_loader, test_loader
+  # If return_weights is true, calculates and returns class weights, otherwise only returns DataLoaders
+  if return_weights:
+    class_weights = get_weights(y_train)
+    return train_loader, test_loader, class_weights
+  else:
+    return train_loader, test_loader
 
 def test_loop(model: TCNModel, loader, epoch: int, loss_fn, acc_metric):
   """
@@ -39,6 +59,7 @@ def test_loop(model: TCNModel, loader, epoch: int, loss_fn, acc_metric):
   acc_metric.reset()
   # Use torch.inference_mode to disable gradient tracking
   with torch.inference_mode():
+    # Calculates loss and accuracy using training Dataset
     for x_ts, y_ts in loader:
       x_ts, y_ts = x_ts.to(DEVICE), y_ts.to(DEVICE)
       x_ts = x_ts.transpose(1, 2)
@@ -56,10 +77,8 @@ def train_loop(model: TCNModel, epochs: int, optimizer, loss_fn, scheduler, acc_
   """
   Function to perform the training loop
   """
-  train_loader, test_loader = get_loaders()
-  """
-  Training Loop
-  """
+  train_loader, test_loader, weights = get_loaders()
+
   for epoch in range(epochs):
     model.train() # Sets model to training mode
     total_loss = 0.0 # Stores cumulative loss for each batch
