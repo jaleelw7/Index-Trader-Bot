8b71dd85c7424956535e37b82613d64ed858209a
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-13 22:13:09 -0400

    Fixed error with assigning tensor device

--- backend/service/model_inference.py
+++ backend/service/model_inference.py
@@ -7,15 +7,16 @@ from model.model_tcn import TCNModel
 # Create a new model instance and load the saved state_dict
 # Only load model once on import
 MODEL = TCNModel().to(DEVICE)
-MODEL.load_state_dict(torch.load(SAVE_PATH, weights_only=True))
+MODEL.load_state_dict(torch.load(SAVE_PATH, weights_only=True, map_location=DEVICE))
 MODEL.eval() # Set to evaluation mode for predictions
 
 def pass_input(X: torch.Tensor) -> dict:
   """
   Passes input tensor into the saved model
   """
+  X = X.to(DEVICE) # Move tensor to correct device
+
   with torch.inference_mode():
-    X.to(DEVICE) # Move tensor to correct device
     X = X.transpose(1, 2) # Transpose tensor to correct shape
     logits = MODEL(X)
     probs = torch.softmax(logits, dim=1) # Get probabilities for each class
