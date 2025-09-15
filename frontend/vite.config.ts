4e7cd5e6f910b114727ee343c5c02ce5e99910b4
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-15 18:08:50 -0400

    Created vite config

---
+++ frontend/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // Proxy server to avoid CORS
+  server: {
+    proxy: {"/api": "http://localhost8000"}
+  }
+});
