5524495490b2e578bae0d7bacf89c5a83e3aa4ac
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-19 20:02:51 -0400

    Normalized Datetime/Date column from yfinance dataframe to support periods beyond 3 months

--- backend/service/candles.py
+++ backend/service/candles.py
@@ -11,8 +11,19 @@ def get_candles(ticker: str, interval: str = "30m", period: str = "1mo") -> pd.D
   if df is None or df.empty: return df  # Prevents errors from operating on empty or null DataFrame
 
   df.reset_index(inplace=True) # Replace Datetime index with numerical index
-  df.sort_values("Datetime", inplace=True) # Sort dataframe by datetime
-  display_features = ["Datetime", "Open", "High", "Low", "Close", "Volume"]
+  # Rename Datetime/Date column to timestamp
+  if "Datetime" in df.columns:
+    df = df.rename(columns={"Datetime": "timestamp"})
+  elif "Date" in df.columns:
+    df = df.rename(columns={"Date": "timestamp"})
+  else:
+    raise KeyError("No Datetime/Date column found")
+  
+  # # Ensure timestamp column is ISO strings and drop timezone
+  # df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.tz_localize(None).astype(str)
+   
+  df = df.sort_values("timestamp") # Sort dataframe by timestamp
+  display_features = ["timestamp", "Open", "High", "Low", "Close", "Volume"]
   return df[display_features].dropna()
 
 def serialize_candles(df: pd.DataFrame) -> list[dict]:
@@ -21,7 +32,7 @@ def serialize_candles(df: pd.DataFrame) -> list[dict]:
   """
   return [
     {
-      "ts": r.Datetime.isoformat(),
+      "ts": r.timestamp.tz_localize(None).isoformat(),
       "open": float(r.Open),
       "high": float(r.High),
       "low": float(r.Low),
