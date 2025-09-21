f7da192202ca105b71ac88a7cfccb823abd4eb2d
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-20 21:09:34 -0400

    Added CORS origins

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from .routes import api_bp
 from .error_handlers import register_error_hadlers
+from backend.config import CORS_ORIGINS
 
 def create_app() -> Flask:
   """
   Application factory
   """
   app = Flask(__name__)
-  CORS(app) # Configure origins
+  CORS(app, origins=CORS_ORIGINS) # Configure origins
   app.register_blueprint(api_bp, url_prefix="/api")
   register_error_hadlers(app)
   return app
