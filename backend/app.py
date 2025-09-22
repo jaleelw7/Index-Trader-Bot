d864734838f98bdd7f98c23d7231d09695cbbd20
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-22 17:35:14 -0400

    Changed entry point to local dev only

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,9 @@ import os
 from backend.api import create_app
 from backend.config import API_HOST, API_PORT
 
-"""
-API entry point
-"""
-app = create_app()
-
 if __name__ == "__main__":
-
+  # Entry point for local  dev
+  app = create_app()
   server = os.getenv("APP_SERVER", "werkzeug").lower()
   # For Windows
   if server == "waitress":
