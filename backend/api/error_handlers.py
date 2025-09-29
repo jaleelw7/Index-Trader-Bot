5a1cd96d5eb50990ea33342a144ef081f8279f15
Jaleel Williams <jaleelcollin18@gmail.com>
2025-09-29 04:35:15 -0400

    Added error handlers for custom errors and KeyError

--- backend/api/error_handlers.py
+++ backend/api/error_handlers.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from backend.config import ApiKeyError, DownloadError
 
 def register_error_hadlers(app):
   """
@@ -11,3 +12,15 @@ def register_error_hadlers(app):
   @app.errorhandler(500)
   def server_error(e):
     return jsonify({"error": "Server error", "details": str(e)}), 500
+  
+  @app.errorhandler(ApiKeyError)
+  def api_key_error(_):
+    return jsonify({"error": "API key invalid or missing."}), 500
+  
+  @app.errorhandler(DownloadError)
+  def tiingo_download_error(e: DownloadError):
+    return jsonify({"error": "Tiingo download failed", "details": str(e)}), 500
+  
+  @app.errorhandler(KeyError)
+  def df_key_error(_):
+    return jsonify({"error": "DataFrame column error"}), 500
